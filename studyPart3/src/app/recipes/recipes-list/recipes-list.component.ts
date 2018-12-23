d534f2d495a55099fe9f80c40494a84c5f0e8a00
amorfos33 <amorfos33@gmail.com>
2018-12-23 20:25:59 +0200

    part 3 , lection 50

--- studyPart3/src/app/recipes/recipes-list/recipes-list.component.ts
+++ studyPart3/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Thi is simple test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg'),
+    new Recipe('A test Recipe', 'Thi is simple test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
