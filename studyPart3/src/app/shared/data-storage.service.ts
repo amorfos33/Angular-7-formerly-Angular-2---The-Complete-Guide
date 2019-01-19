ae83ff1e1853cc38bf1b239319aca97e10e91ca2
amorfos33 <amorfos33@gmail.com>
2019-01-20 01:02:37 +0200

    start part 20

--- studyPart3/src/app/shared/data-storage.service.ts
+++ studyPart3/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+// tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
 
 @Injectable()
@@ -15,6 +16,7 @@ export class DataStoregeService {
         .map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
+                // tslint:disable-next-line:prefer-const
                 for (let recipe of recipes) {
                     if (!recipe['ingredients']) {
                         console.log(recipe);
@@ -25,7 +27,7 @@ export class DataStoregeService {
             }
         )
         .subscribe(
-            (recipes: Recipe) => {
+            (recipes: Recipe[]) => {
                 this.recipeService.setRecipe(recipes);
             }
         );
