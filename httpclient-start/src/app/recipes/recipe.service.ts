7d246fc6b20143e7512ae57c2eeddb1ecbca4150
amorfos33 <amorfos33@gmail.com>
2019-02-06 12:39:16 +0200

    part 23 lecture 300

---
+++ httpclient-start/src/app/recipes/recipe.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'Thi is simple test',
+// tslint:disable-next-line:max-line-length
+'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg',
+      [
+          new Ingredient('Meate', 1),
+          new Ingredient('French Fries', 20)
+      ]),
+  new Recipe(
+      'A test Recipe',
+      'Thi is simple test',
+'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg',
+      [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meate', 1)
+      ])
+];
+
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
---
+++ httpclient-start/src/app/shared/data-storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+// tslint:disable-next-line:import-blacklist
+import 'rxjs/Rx';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
+
+  storeRecipes() {
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-e8d2e.firebaseio.com/recipe.json?auth=' + token, this.recipeService.getRecipes());
+    }
+    getRecipe() {
+        const token = this.authService.getToken();
+        return this.http.get('https://ng-recipe-book-e8d2e.firebaseio.com/recipe.json?auth=' + token)
+        .map(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                // tslint:disable-next-line:prefer-const
+                for (let recipe of recipes) {
+                    if (!recipe['ingredients']) {
+                        console.log(recipe);
+                        recipe['ingredients'] = [];
+                    }
+                }
+                return recipes;
+            }
+        )
+        .subscribe(
+            (recipes: Recipe[]) => {
+                this.recipeService.setRecipes(recipes);
+            }
+        );
+    }
+}
